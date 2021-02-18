10f4610e4526d475f2b383f25541053c75443ce1
Abhisonu21 <49618237+Abhisonu21@users.noreply.github.com>
2021-02-18 18:05:41 +0530

    Update Tabcontent.js

--- src/Components/Tabcontent.js
+++ src/Components/Tabcontent.js
@@ -7,11 +7,11 @@ const TabContent = (props) => {
     return(
         <div className="tab-item-wrapper">
             <div className="tab-head">
-                <img src={props.main_user} alt={props.user_name} className="img-fluid display-inline margin-right-10" style={{width: '80px', float: 'left'}} /> 
+                <img src={props.main_user} alt={props.user_name} className="img-fluid display-inline margin-right-10 img-sec"/> 
                 
-                <span><b>{ props.user_name } <i className="fa fa-map-marker margin-right-10 margin-left-20" style={{ display: isValue ? "none" : "inline-block"  }}/>{props.location}</b> 
+                <span className="margin-bottom-10"><b>{ props.user_name } <i className="fa fa-map-marker margin-right-10 margin-left-20" style={{ display: isValue ? "none" : "inline-block"  }}/>{props.location}</b> 
                 
-                <a href="#" class="btn btn-primary margin-left-15" style={{ display: isbtnName ? "none" : "inline-block", padding: '1px 15px' }}>{props.aname}</a><br/>{props.subject}</span>
+                <a href="#" class="btn btn-primary margin-left-15 margin-top-10" style={{ display: isbtnName ? "none" : "inline-block", padding: '1px 15px' }}>{props.aname}</a><br className="margin-top-10"/>{props.subject} <b>{props.subject1}</b></span>
 
                 <p>{props.duration}</p>
 
